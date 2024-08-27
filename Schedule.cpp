8a8e19b2901e616d991b7294ea97fd81a1847270
Liemani <parkjh2768@gmail.com>
2024-08-27 09:01:17 +0900

    Replace pid to getpid() rather than 0

--- Schedule.cpp
+++ Schedule.cpp
@@ -2,11 +2,12 @@
 
 #include <sched.h>
 #include <cassert>
+#include <unistd.h>
 
 void setSchedulePriorityMax() {
   struct sched_param schedulerParameter;
   const int schedulerPriorityMax = sched_get_priority_max(SCHED_FIFO);
   assert(schedulerPriorityMax != -1);
   schedulerParameter.sched_priority = schedulerPriorityMax;
-  assert(sched_setscheduler(0, SCHED_FIFO, &schedulerParameter) != -1);
+  assert(sched_setscheduler(getpid(), SCHED_FIFO, &schedulerParameter) != -1);
 }
